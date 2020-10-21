a3074d728756a3c64118a50c1421e5b7c8dc16f3
Mark Rizko <mark@rizkocircle.com>
2020-10-21 11:28:29 -0700

    added two commands

--- bot.py
+++ bot.py
@@ -3,26 +3,32 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+import random
+
+bot = commands.Bot(command_prefix = '!')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
 
-@client.event
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(f'{client.user} has connected to ' + f'{guild.name}!')
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(f'{bot.user} has connected to ' + f'{guild.name}!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    escape = 'Вы должны сбежать из Таркова!'
+@bot.command()
+async def escape(ctx):
+    await ctx.send('Вы должны сбежать из Таркова!')
 
-    if message.content == '!escape':
-        await message.channel.send(escape)
+@bot.command()
+async def flip_coin(ctx):
+    coin = random.random()
+    # print(coin)
+    if coin < 0.5:
+        await ctx.send('Heads')
+    else:
+        await ctx.send('Tails')
 
-client.run(TOKEN)
+bot.run(TOKEN)
